734eb6dc50d2703b09d63ea042805f810931524b
leocsas <leocristian2014@gmail.com>
2025-10-13 22:52:00 -0300

    Testes aula 15.

---
+++ testes/aula15/teste01.py
@@ -0,0 +1,13 @@
+'''cont = 1
+while cont <= 10:
+    print(cont, '-> ', end='')
+    cont += 1
+print('ACABOU!')'''
+
+cont = 1
+while True:
+    print(cont, '-> ', end='')
+    cont += 1
+print('ACABOU!')
+
+# Executa para sempre, break quebra o loop -> teste02
